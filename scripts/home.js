b0094ce7823b7bf7142f1384e3647678341ae1ac
Thomas Chia <tchia97@gmail.com>
2023-07-02 13:57:34 +0100

    Add bubble to home page.

---
+++ scripts/home.js
@@ -0,0 +1,10 @@
+// const bubble = document.querySelector('.bubble');
+//     let direction = 1;
+//     let position = 50;
+//     setInterval(() => {
+//       position += direction;
+//       if (position >= 70 || position <= 40) {
+//         direction *= -1;
+//       }
+//       bubble.style.top = `${position}%`;
+//     }, 40);
