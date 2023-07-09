4a95d85c164d10570f0e3f0fc1c793f009a4faa1
Thomas Chia <tchia97@gmail.com>
2023-07-09 11:16:29 +0100

    Add test files for drawing a tree diagram.

---
+++ scripts/tree_diagram.js
@@ -0,0 +1,23 @@
+const pythonSkillData = {
+    name: 'Python',
+    children: [
+        { name: 'PyTorch' },
+        { name: 'Pandas' },
+        { name: 'Flask' },
+    ]};
+
+const sqlSkillData = {
+    name: 'Database',
+    children: [
+        { name: 'MySQL' },
+        { name: 'PostgreSQL' },
+        { name: 'MongoDB' },
+    ]};
+
+const frontendSkillData = {
+    name: 'Frontend',
+    children: [
+        { name: 'HTML' },
+        { name: 'CSS' },
+        { name: 'JavaScript' },
+    ]};
