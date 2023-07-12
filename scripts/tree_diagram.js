fbca80076419019005ed9a583b55916682c58039
Thomas Chia <tchia97@gmail.com>
2023-07-12 15:57:43 +0100

    Add tree diagram test file.

--- scripts/tree_diagram.js
+++ scripts/tree_diagram.js
@@ -1,23 +1,47 @@
-const pythonSkillData = {
-    name: 'Python',
-    children: [
-        { name: 'PyTorch' },
-        { name: 'Pandas' },
-        { name: 'Flask' },
-    ]};
+import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 
-const sqlSkillData = {
-    name: 'Database',
-    children: [
-        { name: 'MySQL' },
-        { name: 'PostgreSQL' },
-        { name: 'MongoDB' },
-    ]};
+const pythonSkills = [
+    "Python/PyTorch",
+    "Python/Pandas",
+    "Python/Flask",
+]
 
-const frontendSkillData = {
-    name: 'Frontend',
-    children: [
-        { name: 'HTML' },
-        { name: 'CSS' },
-        { name: 'JavaScript' },
-    ]};
+const databaseSkills = [
+    "SQL/MySQL",
+    "SQL/PostgreSQL",
+    "SQL/MongoDB",
+]
+
+const frontendSkills = [
+    "Frontend/HTML",
+    "Frontend/CSS",
+    "Frontend/JavaScript",
+]
+
+const pythonPlot =Plot.plot({
+    axis: null,
+    height: 100,
+    margin: 10,
+    marginLeft: 40,
+    marginRight: 120,
+    marks: [
+        Plot.tree(pythonSkills, {textStroke: "white"})
+    ]
+    })
+
+const databasePlot =Plot.plot({
+    axis: null,
+    height: 100,
+    margin: 10,
+    marginLeft: 40,
+    marginRight: 120,
+    marks: [
+        Plot.tree(databaseSkills, {textStroke: "white"})
+    ]
+    })
+
+// const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+const div = document.getElementById("myplot");
+div.append(pythonPlot);
+div.append(databasePlot);
+console.log("Hello from tree_diagram.js")
